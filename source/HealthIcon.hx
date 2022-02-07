3637a79d5c1d0828ba5f61ec70344175101b9aaa
TheOrda <91075908+TheOrda@users.noreply.github.com>
2022-02-07 18:18:23 -0300

    Add files via upload

--- source/HealthIcon.hx
+++ source/HealthIcon.hx
@@ -1,5 +1,8 @@
 package;
 
+import flixel.FlxG;
+import flixel.graphics.FlxGraphic;
+import flixel.graphics.frames.FlxAtlasFrames;
 import flixel.FlxSprite;
 import openfl.utils.Assets as OpenFlAssets;
 
@@ -10,7 +13,10 @@ class HealthIcon extends FlxSprite
 	public var sprTracker:FlxSprite;
 	private var isOldIcon:Bool = false;
 	private var isPlayer:Bool = false;
-	private var char:String = '';
+	public var char:String = '';
+
+	public var offsetX = 0;
+	public var offsetY = 0;
 
 	public function new(char:String = 'bf', isPlayer:Bool = false)
 	{
@@ -34,23 +40,49 @@ class HealthIcon extends FlxSprite
 		else changeIcon('bf');
 	}
 
+	private var iconOffsets:Array<Float> = [0, 0];
 	public function changeIcon(char:String) {
 		if(this.char != char) {
-			var name:String = 'icons/' + char;
-			if(!Paths.fileExists('images/' + name + '.png', IMAGE)) name = 'icons/icon-' + char; //Older versions of psych engine's support
-			if(!Paths.fileExists('images/' + name + '.png', IMAGE)) name = 'icons/icon-face'; //Prevents crash from missing icon
-			var file:Dynamic = Paths.image(name);
+			offsetX = 0;
+			offsetY = 0;
+			switch (char) {
+				case 'tankman' | 'tankmanlol':
+					var file:FlxAtlasFrames = Paths.getSparrowAtlas('icons/TankmanIcon');
+					frames = file;
+					
+					animation.addByPrefix(char, 'TankmanIcon', 24, true);
+					animation.play(char);
 
-			loadGraphic(file, true, 150, 150);
-			animation.add(char, [0, 1], 0, false, isPlayer);
-			animation.play(char);
+					offsetY += 5;
+				default:
+					var name:String = 'icons/' + char;
+					if(!Paths.fileExists('images/' + name + '.png', IMAGE)) name = 'icons/icon-' + char; //Older versions of psych engine's support
+					if(!Paths.fileExists('images/' + name + '.png', IMAGE)) name = 'icons/icon-face'; //Prevents crash from missing icon
+
+					// lmao dont mind me just importing forever's dynamic healthicons
+					var iconGraphic:FlxGraphic = FlxG.bitmap.add(Paths.image(name));
+					loadGraphic(iconGraphic, true, Std.int(iconGraphic.width / 2), iconGraphic.height);
+			
+					animation.add(char, [0, 1], 0, false, isPlayer);
+					animation.play(char);
+					updateHitbox();
+			}
 			this.char = char;
+
+			updateHitbox();
 
 			antialiasing = ClientPrefs.globalAntialiasing;
 			if(char.endsWith('-pixel')) {
 				antialiasing = false;
 			}
 		}
+	}
+
+	override function updateHitbox()
+	{
+		super.updateHitbox();
+		offset.x = iconOffsets[0];
+		offset.y = iconOffsets[1];
 	}
 
 	public function getCharacter():String {
